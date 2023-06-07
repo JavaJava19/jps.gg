02dcc6efa6beb977b97e6cd8ec902cc0f84d8e41
graycat27 <64903817+graycat27@users.noreply.github.com>
2023-06-08 04:01:11 +0900

    revert a part of f95f7630 `Attempted import error: 'react-router-dom' does not contain a default export (imported as 'Link').` Action message

--- source/src/components/page/Read.js
+++ source/src/components/page/Read.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import style from "../../css/read.module.css"
-import Link from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Read = () => {
 
