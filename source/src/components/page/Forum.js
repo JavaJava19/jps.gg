0ce83bfa3b62458cfc1d02e317cdfcf11f122276
kaogon128 <bump_love128@yahoo.co.jp>
2023-06-15 00:39:42 +0900

    とことんWarning消したる

--- source/src/components/page/Forum.js
+++ source/src/components/page/Forum.js
@@ -1,8 +1,5 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import style from "../../css/forum.module.css"
-import { useCookies } from 'react-cookie';
-import useFetchAddress from '../util/FetchAddress';
-import { isLogin, login } from '../util/LoginUtil';
 
 const Forum = () => {
 
