0101dd76a4cbd12613257060f6d907199db19319
graycat27 <64903817+graycat27@users.noreply.github.com>
2023-10-10 15:04:05 +0900

    temp公開用

--- source/src/components/parts/game/Survival/20231009/CitizenGuid.js
+++ source/src/components/parts/game/Survival/20231009/CitizenGuid.js
@@ -8,54 +8,10 @@ const CitizenGuid = () => {
         <h1 className={styles.GameTitle}>一般住民向けガイド</h1>
         <h2>JPS Townyの使い方</h2>
         <p>このガイドは一般市民向けです。町や国の管理者は<a href="./MayorGuid">運営者向けガイド</a>の確認をお願いします。</p>
-        <nav>
-          <ol>
-            <li><a href="#s2_n1">町と国</a></li>
-            <li><a href="#s2_n2">できること</a></li>
-            <li><a href="#s2_n3">町のルール</a></li>
-            <li><a href="#s2_n4">コマンド</a></li>
-            <li><a href="#s2_n5">用語集</a></li>
-            <li><a href="#s2_link">関連リンク</a></li>
-          </ol>
-        </nav>
+        <p>Discord掲載のPDFをご参照ください</p>
       </header>
-      <main className={styles.GameContainer}>
-        <section id="s2_n1">
-          <h2>1. 町と国</h2>
-          <p>
-
-          </p>
-        </section>
-        <section id="s2_n2">
-          <h2>2. できること</h2>
-          <p>
-
-          </p>
-        </section>
-        <section id="s2_n3">
-          <h2>3. 町のルール</h2>
-          <p>
-
-          </p>
-        </section>
-        <section id="s2_n4">
-          <h2>4. コマンド</h2>
-          <p>
-
-          </p>
-        </section>
-        <section id="s2_n5">
-          <h2>5. 用語集</h2>
-          <p>
-
-          </p>
-        </section>
-        <section id="s2_link">
-          <h2>関連リンク</h2>
-          <p>
-
-          </p>
-        </section>
+      <main>
+        <p>現在ウェブ掲載用に編集中</p>
       </main>
     </article>
     )
