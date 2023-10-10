48b4e7ad0871a5b4028ba5bc6f959e60fd492c68
graycat27 <64903817+graycat27@users.noreply.github.com>
2023-10-10 23:42:52 +0900

    見出しの更新

--- source/src/components/parts/game/Survival/20231009/CitizenGuid.js
+++ source/src/components/parts/game/Survival/20231009/CitizenGuid.js
@@ -10,51 +10,61 @@ const CitizenGuid = () => {
         <p>このガイドは一般市民向けです。町や国の管理者は<a href="./MayorGuid">運営者向けガイド</a>の確認をお願いします。</p>
         <nav>
           <ol>
-            <li><a href="#s2_n1">町と国</a></li>
-            <li><a href="#s2_n2">できること</a></li>
-            <li><a href="#s2_n3">町のルール</a></li>
-            <li><a href="#s2_n4">コマンド</a></li>
-            <li><a href="#s2_n5">用語集</a></li>
+            <li><a href="#s2_n1">町に入ろう！</a></li>
+            <li><a href="#s2_n2">町を発展させよう！</a></li>
+            <li><a href="#s2_n3">JPを稼ごう！</a></li>
+            <li><a href="#s2_n4">自分で町をつくってみよう！</a></li>
+            <li><a href="#s2_n5">タウニー用語</a></li>
+            <li><a href="#s2_n6">コマンド一覧</a></li>
             <li><a href="#s2_link">関連リンク</a></li>
           </ol>
         </nav>
       </header>
       <main className={styles.GameContainer}>
         <section id="s2_n1">
-          <h2>1. 町と国</h2>
+          <h2>1. 町に入ろう！</h2>
           <p>
 
           </p>
         </section>
         <section id="s2_n2">
-          <h2>2. できること</h2>
+          <h2>2. 町を発展させよう！</h2>
           <p>
 
           </p>
         </section>
         <section id="s2_n3">
-          <h2>3. 町のルール</h2>
+          <h2>3. JPを稼ごう！</h2>
           <p>
 
           </p>
         </section>
         <section id="s2_n4">
-          <h2>4. コマンド</h2>
+          <h2>4. 自分で町をつくってみよう！</h2>
           <p>
 
           </p>
         </section>
         <section id="s2_n5">
-          <h2>5. 用語集</h2>
+          <h2>5. タウニー用語</h2>
+          <p>
+
+          </p>
+        </section>
+        <section id="s2_n6">
+          <h2>5. コマンド一覧</h2>
           <p>
 
           </p>
         </section>
         <section id="s2_link">
           <h2>関連リンク</h2>
-          <p>
-
-          </p>
+          <nav>
+            <ul>
+              <li><a href="./RenewGuid">23年10月9日更新情報</a></li>
+              <li><a href="./MayorGuid">町と国の管理者向けガイド</a></li>
+            </ul>
+          </nav>
         </section>
       </main>
     </article>
