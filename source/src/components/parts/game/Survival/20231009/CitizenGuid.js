7864e06c0255d3dd7d2c4c3bb5563a1f7a0f52fb
graycat27 <64903817+graycat27@users.noreply.github.com>
2023-10-13 01:29:56 +0900

    画像がデカすぎてPCでもレイアウト崩れてた

--- source/src/components/parts/game/Survival/20231009/CitizenGuid.js
+++ source/src/components/parts/game/Survival/20231009/CitizenGuid.js
@@ -188,7 +188,7 @@ const CitizenGuid = () => {
             <tr><td><code>/g</code></td><td>チャット送信先を、建築サーバ全体に切り替えます</td>
               <td rowSpan="5"><img
                   src="https://user-images.githubusercontent.com/64903817/274629466-c39d213c-44cb-4653-a8ea-60a74a4269ed.png"
-                  alt="チャット送信先を切り替えた時の通知の表示事例" height="476px"/></td></tr>
+                  alt="チャット送信先を切り替えた時の通知の表示事例" height="200px" style={{margin:"3px"}}/></td></tr>
             <tr><td><code>/l</code></td><td>チャット送信先を、ローカルに切り替えます（半径100マス以内）</td></tr>
             <tr><td><code>/tc</code></td><td>チャット送信先を、町のメンバー限定に切り替えます</td></tr>
             <tr><td><code>/nc</code></td><td>チャット送信先を、自国のメンバー限定に切り替えます</td></tr>
