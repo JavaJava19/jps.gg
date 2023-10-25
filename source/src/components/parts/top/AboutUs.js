b6891c93f4ceccd33169396fb240e6cae0b72841
graycat27 <64903817+graycat27@users.noreply.github.com>
2023-10-25 19:55:52 +0900

    サーバをサーバーに

--- source/src/components/parts/top/AboutUs.js
+++ source/src/components/parts/top/AboutUs.js
@@ -6,19 +6,19 @@ const AboutUs = () => {
         <section className={style.AboutUs}>
         <section className={style.AboutUsContent}>
             <h2>Japan PvP Server (JPS) へようこそ</h2>
-            <p>JPSは、Discordサーバを中心に、24時間常時開放のMinecraftサーバなどで形成されているコミュニティです</p>
+            <p>JPSは、Discordサーバーを中心に、24時間常時開放のMinecraftサーバーなどで形成されているコミュニティです</p>
             <p>コミュニティの核は、<ruby>あかさん<rp>(</rp><rt>az_aka</rt><rp>)</rp></ruby>
                 & <ruby>あおさん<rp>(</rp><rt>az__ao</rt><rp>)</rp></ruby>の二人で、
                 毎週2回以上の視聴者参加型Minecraftイベント企画をYouTubeで配信しています。</p>
             <section>
-                <h3>Discordサーバ</h3>
+                <h3>Discordサーバー</h3>
                 <p><a href={"https://discord.app/"}>Discord</a>は無料で利用可能なコミュニケーションアプリです。ブラウザ版・スマホアプリ版が存在しています。<br/>
                    <a href={"https://discord.gg/jps"}>参加リンク</a>からJPS参加型サーバへ参加してください</p>
             </section>
             <section>
-                <h3>Minecraftサーバ</h3>
-                <p>JPSのMinecraftサーバは、Java版に限らず、統合版（Switch,PS5他）も参加可能！</p>
-                <p>24時間常時開放のサーバには、各種ミニゲーム（CatchTheWool,BedWars,ThePit,Diggerなど）と、サバイバルサーバ群が用意されています。<br/>
+                <h3>Minecraftサーバー</h3>
+                <p>JPSのMinecraftサーバーは、Java版に限らず、統合版（Switch,PS5他）も参加可能！</p>
+                <p>24時間常時開放のサーバーには、各種ミニゲーム（CatchTheWool,BedWars,ThePit,Diggerなど）と、サバイバルサーバ群が用意されています。<br/>
                     <a href={"/game"}>ミニゲーム等の詳細</a>もご覧ください</p>
             </section>
             <section>
