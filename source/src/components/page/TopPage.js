bdb4ef4d04fe99df6b862b58e7985816c08ad028
graycat27 <64903817+graycat27@users.noreply.github.com>
2023-11-18 08:19:43 +0900

    append to TopPage

--- source/src/components/page/TopPage.js
+++ source/src/components/page/TopPage.js
@@ -4,9 +4,18 @@ import Slider from '../parts/top/Slider';
 import WelcomePops from '../parts/top/WelcomePops';
 import TopForum from '../parts/top/TopForum';
 import AboutUs from '../parts/top/AboutUs';
+import UpdateOgp from '../util/UpdateOgp';
+
 const TopPage = () => {
 
   return (
+    <UpdateOgp props={pageMeta={
+      {
+        title: 'Top | JapanPvPServer',
+        description: 'Welcome to JPS! DiscordとMinecraftのコミュニティサーバーへようこそ',
+        url: 'https://jps.gg/'
+      }
+    }}/>
     <main>
       <div className={style.SlideField}>
         <Slider props={<WelcomePops/>}/>
